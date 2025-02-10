ea04d21f8347b54b6fb1af40740c6c8dd6c0f16a
Christoffer <mech1803@gmail.com>
2025-02-10 23:18:24 +0100

    Add several methods to Ui class

--- src/js/Ui.js
+++ src/js/Ui.js
@@ -1,9 +1,35 @@
 class Ui {
-  static renderPager(id) {
-    if (id === "students") {
-        
-    } else if (id === "Instructors") {
-    } else if (id === "courses") {
-    }
+  //Add this to all nav buttonsj and pass the dataset id to the render page
+  static renderPage(id) {
+    this.clearPage();
+    this.createPanel(id);
+    //this.renderData(id);
+  }
+
+  static createPanel(id) {
+    console.log(id);
+    const panel = document.querySelector(".panel");
+    const title = document.createElement("h2");
+    title.classList.add("panel__title");
+    title.textContent = id;
+
+    const addButton = document.createElement("button");
+    addButton.classList.add("panel__add");
+    addButton.textContent = `add ${id}`;
+    panel.append(title, addButton);
+  }
+
+  static renderData(id) {
+    const data = localStorage.getItem(id);
+  }
+
+  static clearPage() {
+    const panel = document.querySelector(".panel");
+    panel.innerHTML = "";
+
+    const data = document.querySelector(".data");
+    data.innerHTML = "";
   }
 }
+
+export default Ui;
