418ee0081346771a84df0b0c91a22c95293442d1
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-02-17 19:55:34 +0100

    update to student.js

--- src/js/student.js
+++ src/js/student.js
@@ -1,10 +1,14 @@
 import Person from "./person";
 
 class Student extends Person {
-    constructor(name, email, phone, address, enrollCourse) {
+    constructor(name, email, phone, address) {
         super(name, email, phone, address);
-        this.enrollCourse = enrollCourse;
+        this.enrolledCourses = [];
         
+    }
+
+    enrollCourse(course) {
+        this.enrolledCourses.push(course);
     }
 }
 
